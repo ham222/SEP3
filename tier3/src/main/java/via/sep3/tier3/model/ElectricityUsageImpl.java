3d24aede4d5c3e3a3095680994d97464f48837ba
Iurie Gavriliuc <iurg2002@gmail.com>
2022-11-01 22:07:56 +0100

    GRPC implimentation
    Tests are available with BloomRPC

--- tier3/src/main/java/via/sep3/tier3/model/ElectricityUsageImpl.java
+++ tier3/src/main/java/via/sep3/tier3/model/ElectricityUsageImpl.java
@@ -1,4 +1,57 @@
 package via.sep3.tier3.model;
 
 public class ElectricityUsageImpl {
+
+    private int id;
+    private double amount;
+    private int month;
+    private int year;
+
+    public ElectricityUsageImpl(int id, double amount, int month, int year)
+    {
+        this.id = id;
+        this.amount = amount;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(double amount)
+    {
+        this.amount = amount;
+    }
+
+    public int getMonth()
+    {
+        return month;
+    }
+
+    public void setMonth(int month)
+    {
+        this.month = month;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
 }
--- tier3/src/main/java/via/sep3/tier3/model/WaterUsageImpl.java
+++ tier3/src/main/java/via/sep3/tier3/model/WaterUsageImpl.java
@@ -1,4 +1,56 @@
 package via.sep3.tier3.model;
 
 public class WaterUsageImpl {
+    private int id;
+    private double amount;
+    private int month;
+    private int year;
+
+    public WaterUsageImpl(int id, double amount, int month, int year)
+    {
+        this.id = id;
+        this.amount = amount;
+        this.month = month;
+        this.year = year;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public double getAmount()
+    {
+        return amount;
+    }
+
+    public void setAmount(double amount)
+    {
+        this.amount = amount;
+    }
+
+    public int getMonth()
+    {
+        return month;
+    }
+
+    public void setMonth(int month)
+    {
+        this.month = month;
+    }
+
+    public int getYear()
+    {
+        return year;
+    }
+
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
 }
